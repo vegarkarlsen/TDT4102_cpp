09b4af8911c2ba6c695a2d5ae79993a808bab9fc
vegar <vegar1201@live.no>
2023-03-09 23:05:04 +0100

    sudocode for first task

---
+++ oving_8/main.cpp
@@ -0,0 +1,47 @@
+
+#include <iostream>
+#include <vector>
+#include <array>
+
+
+
+
+// void fillInFibonacciNumbers(int result, int lenght){
+//     int a = 0;
+//     int b = 1;
+//     int s;
+//     std::vector<int> test;
+//     for (int i = 2; i < lenght; i++){
+
+//         if (i <= lenght){
+//             s = a + b;
+//             test.push_back(s);
+//             a = b;
+//             b = s;
+//             std::cout << s << std::endl;
+//         }
+//     }
+    
+//     std::cout << "print vector" << std::endl;
+//     for (auto t : test){
+//         std::cout << t << std::endl;
+//     }
+
+// }
+
+void fillInFibonacciNumbers(int *result, int lenght){
+    
+    
+}
+
+
+
+
+
+int main(){
+    // fillInFibonacciNumbers(1,10);
+    
+
+
+    return 0;
+}
